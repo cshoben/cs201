c2d2471352b92d0bc2576580b2a8c8ae34a9e562
Chelsea Shoben <chelsea.shoben@duke.edu>
2022-09-09 13:23:51 -0400

    saving work from other day in Projects for p0

---
+++ Projects/p0-person201/src/Person201Finder.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.util.List;
+
+public class Person201Finder {
+    public static void main(String[] args) throws IOException{
+        Person201 myself = new Person201("Chelsea Shoben", "CARL", 4);
+        Person201[] cs201 = Person201Utilities.readURL("https://courses.cs.duke.edu/compsci201/fall22/people.txt");
+
+        System.out.println("\nSearching for people near " + myself.getName() + "\n");
+
+        System.out.println("People on the same floor: ");
+        List<Person201> sameFloor = Person201Utilities.sameFloor(cs201, myself);
+        Person201Utilities.printPeople(sameFloor);
+
+        System.out.println("People in the same building: ");
+        List<Person201> sameBuilding = Person201Utilities.sameBuilding(cs201, myself);
+        Person201Utilities.printPeople(sameBuilding);
+    }
+}
