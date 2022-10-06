aeeafa3fda339eaa6d3be34c46ebe3d596609104
Chelsea Shoben <chelsea.shoben@duke.edu>
2022-10-05 21:03:01 -0400

    add both Closet.java and SetAside.java which are tested and confirmed on APT website

--- APT/apt5/Closet.java
+++ APT/apt5/Closet.java
@@ -1,6 +1,38 @@
+
+import java.util.*;
+
+
 public class Closet {
     public String anywhere(String[] list) {
-        // replace this with your code
-        return "";
+         // Start with an empty HashSet
+         HashSet<String> uniqueWordsAny = new HashSet<>();
+
+         // Look at each string in the input array, 
+         // add break it into individual words
+         // add these words to the hashSet with addAll method, this will only retain unique words
+         for (String string : list) {
+            String [] words = string.split("\\s+");
+            uniqueWordsAny.addAll(Arrays.asList(words));
+        }
+
+        // Make an ArrayList so we can sort alphabetically. 
+        // Use the Strings.join() method to put all words into one string. 
+        ArrayList<String> wordsSorted = new ArrayList<>(uniqueWordsAny);
+        Collections.sort(wordsSorted);
+
+        String together = String.join(" ", wordsSorted);
+
+        return together;
     }
+
+        /*
+    public static void main(String[] args) {
+        String[] list = {"cow bell", "bell toll", "toll booth"};
+        Closet testInstance = new Closet();
+        String testResult = testInstance.anywhere(list);
+        System.out.println(testResult);   
+        
+    }
+        */
+
 }
--- APT/apt5/SetAside.java
+++ APT/apt5/SetAside.java
@@ -1,6 +1,48 @@
+import java.util.*;
+
+
 public class SetAside {
     public String common(String[] list) {
-        // replace this with your code
-        return "";
+
+        // Start with a map of all the words in the first string. 
+        String[] startingWords = list[0].split("\\s+");
+        HashSet<String> uniqueWordsCommon = new HashSet<>();
+        uniqueWordsCommon.addAll(Arrays.asList(startingWords));
+
+        // Then look at the elemets of the input string[], yes this is 
+        //repeating the first one but whatever for now.
+        // This each string will be split on the words and then HashSet .retainAll() will keep
+        // the intersect of the map and the input arrayList. 
+        for (String string : list) {
+            //char [] word = word.toCharArray();
+            String [] words = string.split("\\s+");
+            uniqueWordsCommon.retainAll(Arrays.asList(words));
+
+        }
+ 
+        // In order to ouput alphabetically, we will turn HashSet to ArrayList and use the .sort() 
+        // method. The .join() methos will then be used to make one long string to be use as the output
+        // of this method. 
+        ArrayList<String> wordsSorted = new ArrayList<>(uniqueWordsCommon);
+        Collections.sort(wordsSorted);
+
+        String together = String.join(" ", wordsSorted);
+
+        return together;
+        
     }
+
+
+
+
+
+        /*
+    public static void main(String[] args) {
+        String[] list = {"blue blue water", "blue skies are blue", "blue is blue"};
+        SetAside testInstance = new SetAside();
+        String testResult = testInstance.common(list);
+        System.out.println(testResult);   
+        
+    }
+        */
 }
